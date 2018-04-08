3074487e3aa83bd54497efae77e7e6e0fe126066
Albert <jkdev123cool@gmail.com>
2018-04-08 15:40:08 +0800

    add JKAlertX.h

---
+++ Example/JKAlert/JKAlertX/JKAlertX.h
@@ -0,0 +1,14 @@
+//
+//  JKAlertX.h
+//  JKAlert
+//
+//  Created by albert on 2018/4/8.
+//  Copyright © 2018年 安永博. All rights reserved.
+//
+
+#ifndef JKAlertX_h
+#define JKAlertX_h
+
+#import "JKAlertView.h"
+
+#endif /* JKAlertX_h */
