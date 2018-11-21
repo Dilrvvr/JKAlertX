ff96099e4bc4ff2696a6481463b3ddebcb040f15
albert <jkcoder123cool@gmail.com>
2018-11-21 18:47:59 +0800

    test

--- Example/JKAlertX/ViewController.m
+++ Example/JKAlertX/ViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (IBAction)customCollectionActionView:(id)sender {
