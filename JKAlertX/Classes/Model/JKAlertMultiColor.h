45aae4ed1272ed098c691a5188df467cf862a987
albertCoder <jkdev123cool@gmail.com>
2020-05-29 11:22:45 +0800

    add class

---
+++ JKAlertX/Classes/Model/JKAlertMultiColor.h
@@ -0,0 +1,16 @@
+//
+//  JKAlertMultiColor.h
+//  JKAlertX
+//
+//  Created by albert on 2020/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JKAlertMultiColor : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JKAlertX/Classes/Model/JKAlertMultiColor.m
@@ -0,0 +1,12 @@
+//
+//  JKAlertMultiColor.m
+//  JKAlertX
+//
+//  Created by albert on 2020/5/29.
+//
+
+#import "JKAlertMultiColor.h"
+
+@implementation JKAlertMultiColor
+
+@end
