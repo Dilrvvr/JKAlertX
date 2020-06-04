038911c1587ca124acbb6f10acbaeaf73e7ff5ae
albertCoder <jkdev123cool@gmail.com>
2020-06-04 22:16:12 +0800

    life cycle

--- JKAlertX/Classes/Category/JKAlertView+LifeCycle.m
+++ JKAlertX/Classes/Category/JKAlertView+LifeCycle.m
@@ -73,4 +73,98 @@
         self.deallocBlock = deallocBlock;
     };
 }
+
+/**
+ * 设置用于通知消失的key
+ * 设置该值后可以使用类方法 JKAlertView.DismissForKey(dismissKey); 来手动消失
+ */
+- (JKAlertView *(^)(NSString *dismissKey))setDismissKey {
+    
+    return ^(NSString *dismissKey) {
+        
+        self.dismissKey = dismissKey;
+        
+        return self;
+    };
+}
+
+/**
+ * 设置是否使JKAlertView.dismissAll(); 对当前JKAlertView无效
+ * 请谨慎使用，若设置为YES 调用JKAlertView.dismissAll(); 将对当前JKAlertView无效
+ */
+- (JKAlertView *(^)(BOOL isNoneffective))setDismissAllNoneffective {
+    
+    return ^(BOOL isNoneffective) {
+        
+        self.isDismissAllNoneffective = isNoneffective;
+        
+        return self;
+    };
+}
+
+/**
+ * 设置用于通知消失的类别
+ * 可以将多个弹框设置同一类别，方便移除同一类别的弹框
+ * 设置该值后可以使用类方法 JKAlertView.dismissForCategory(dismissCategory); 来手动消失
+ */
+- (JKAlertView *(^)(NSString *dismissCategory))setDismissCategory {
+    
+    return ^(NSString *dismissCategory) {
+        
+        self.dismissCategory = dismissCategory;
+        
+        return self;
+    };
+}
+
+/**
+ * 移除设置了dismissKey的JKAlertView
+ * 本质是发送一个通知，让dismissKey为该值的JKAlertView对象执行消失操作
+ */
++ (void(^)(NSString *dismissKey))dismissForKey {
+    
+    return ^(NSString *dismissKey) {
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:JKAlertDismissForKeyNotification object:dismissKey];
+    };
+}
+
+/**
+ * 移除设置了同一dismissCategory的多个JKAlertView
+ * 本质是发送一个通知，让dismissCategory为该值的JKAlertView对象执行消失操作
+ */
++ (void(^)(NSString *dismissCategory))dismissForCategory {
+    
+    return ^(NSString *dismissCategory) {
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:JKAlertDismissForCategoryNotification object:dismissCategory];
+    };
+}
+
+/**
+ * 移除当前所有的JKAlertView
+ * 本质是发送一个通知，让所有的JKAlertView对象执行消失操作
+ * 注意如果某个对象setDismissAllNoneffective为YES时，该对象将不会响应通知
+ * ***谨慎使用该方法***
+ */
++ (void(^)(void))dismissAll {
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:JKAlertDismissAllNotification object:nil];
+    
+    return ^{};
+}
+
+/**
+ * 清空当前所有的JKAlertView
+ * 本质是发送一个通知，让所有的JKAlertView对象执行消失操作
+ * 执行该操作会清空所有的JKAlertView，即使setDismissAllNoneffective为YES亦然，请谨慎操作
+ * ***谨慎使用该方法***
+ */
++ (void(^)(void))clearAll {
+    
+    return ^{
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:JKAlertClearAllNotification object:nil];
+    };
+}
 @end
--- JKAlertX/Classes/View/JKAlertView.h
+++ JKAlertX/Classes/View/JKAlertView.h
@@ -52,25 +52,6 @@
 /** 设置默认的取消action，不需要自带的可以自己设置，不可置为nil */
 @property (nonatomic, copy, readonly) JKAlertView *(^setCancelAction)(JKAlertAction *action);
 
-/**
- * 设置是否使JKAlertView.dismissAll(); 对当前JKAlertView无效
- * 请谨慎使用，若设置为YES 调用JKAlertView.dismissAll(); 将对当前JKAlertView无效
- */
-@property (nonatomic, copy, readonly) JKAlertView *(^setDismissAllNoneffective)(BOOL isNoneffective);
-
-/**
- * 设置用于通知消失的key
- * 设置该值后可以使用类方法 JKAlertView.dismissForKey(dismissKey); 来手动消失
- */
-@property (nonatomic, copy, readonly) JKAlertView *(^setDismissKey)(NSString *dismissKey);
-
-/**
- * 设置用于通知消失的类别
- * 可以将多个弹框设置同一类别，方便移除同一类别的弹框
- * 设置该值后可以使用类方法 JKAlertView.dismissForCategory(dismissCategory); 来手动消失
- */
-@property (nonatomic, copy, readonly) JKAlertView *(^setDismissCategory)(NSString *dismissCategory);
-
 
 #pragma mark
 #pragma mark actionSheet样式
@@ -217,34 +198,6 @@
  * 注意自己计算好自定义HUD的size，以避免横竖屏出现问题
  */
 @property (class, nonatomic, copy, readonly) void (^showCustomHUD)(UIView *(^customHUD)(void), void(^configuration)(JKAlertView *alertView));
-
-/**
- * 移除当前所有的JKAlertView
- * 本质是发送一个通知，让所有的JKAlertView对象执行消失操作
- * 注意如果某个对象setDismissAllNoneffective为YES时，该对象将不会响应通知
- * ***谨慎使用该方法***
- */
-@property (class, nonatomic, copy, readonly) void (^dismissAll)(void);
-
-/**
- * 移除设置了dismissKey的JKAlertView
- * 本质是发送一个通知，让dismissKey为该值的JKAlertView对象执行消失操作
- */
-@property (class, nonatomic, copy, readonly) void (^dismissForKey)(NSString *dismissKey);
-
-/**
- * 移除设置了同一dismissCategory的多个JKAlertView
- * 本质是发送一个通知，让dismissCategory为该值的JKAlertView对象执行消失操作
- */
-@property (class, nonatomic, copy, readonly) void (^dismissForCategory)(NSString *dismissCategory);
-
-/**
- * 清空当前所有的JKAlertView
- * 本质是发送一个通知，让所有的JKAlertView对象执行消失操作
- * 执行该操作会清空所有的JKAlertView，即使setDismissAllNoneffective为YES亦然，请谨慎操作
- * ***谨慎使用该方法***
- */
-@property (class, nonatomic, copy, readonly) void (^clearAll)(void);
 
 
 #pragma mark
