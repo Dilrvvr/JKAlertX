a2c7a1f7f608949055d4dba94a1695c0a1e05acc
albertCoder <jkdev123cool@gmail.com>
2020-07-22 15:38:31 +0800

    test

--- Example/JKAlertX/JKAlertViewController.m
+++ Example/JKAlertX/JKAlertViewController.m
@@ -50,42 +50,6 @@
         
         return label;
         
-    }).setNormalImage([UIImage imageNamed:@"Share_WeChat_Moments"])).addAction([JKAlertAction actionWithTitle:@"朋友圈" style:(JKAlertActionStyleDefault) handler:^(JKAlertAction *action) {
-        
-    }].setCustomView(^(JKAlertAction *action) {
-        
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 200)];
-        label.numberOfLines = 0;
-        label.backgroundColor = [UIColor orangeColor];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.text = @"我也是自定义的view~~3";
-        
-        return label;
-        
-    }).setNormalImage([UIImage imageNamed:@"Share_WeChat_Moments"])).addAction([JKAlertAction actionWithTitle:@"朋友圈" style:(JKAlertActionStyleDefault) handler:^(JKAlertAction *action) {
-        
-    }].setCustomView(^(JKAlertAction *action) {
-        
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 200)];
-        label.numberOfLines = 0;
-        label.backgroundColor = [UIColor orangeColor];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.text = @"我也是自定义的view~~4";
-        
-        return label;
-        
-    }).setNormalImage([UIImage imageNamed:@"Share_WeChat_Moments"])).addAction([JKAlertAction actionWithTitle:@"朋友圈" style:(JKAlertActionStyleDefault) handler:^(JKAlertAction *action) {
-        
-    }].setCustomView(^(JKAlertAction *action) {
-        
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 200)];
-        label.numberOfLines = 0;
-        label.backgroundColor = [UIColor orangeColor];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.text = @"我也是自定义的view~~5";
-        
-        return label;
-        
     }).setNormalImage([UIImage imageNamed:@"Share_WeChat_Moments"])).makeDeallocLogEnabled(YES).show().makeDidDismissHandler(^{
         
         [sender setTitle:@"dismissed" forState:(UIControlStateNormal)];
